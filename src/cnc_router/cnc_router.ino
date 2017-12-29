125a9793efb784279c3c9581d654f144aeeea1fb
Roger Lindsjö <roger@tilialacus.net>
2017-12-30 00:27:56 +0100

    Reduce to only read and display ADC

--- src/cnc_router/cnc_router.ino
+++ src/cnc_router/cnc_router.ino
@@ -1,11 +1,11 @@
 #include <U8g2lib.h>
 
+#include "InterruptADC.h"
+
 // 16 MHz
 
-// Three analoge inputs (x, y, z)
-const int xaxisIn = 0;
-const int yaxisIn = 1;
-const int zaxisIn = 2;
+// For tracking analog in
+InterruptADC adc(3);
 
 // Six digital outputs (step and direction of x, y and z)
 byte motorStates;
@@ -14,7 +14,11 @@ byte xSpeed = 255;
 byte ySpeed = 255;
 byte zSpeed = 255;
 
-const byte MAX_STEPS = 230;
+byte xActual = 255;
+byte yActual = 255;
+byte zActual = 255;
+
+const byte MAX_STEPS = 40;
 byte step;
 // Start in stopped state
 byte xStep = 255;
@@ -26,22 +30,22 @@ byte c = 0;
 
 // TWI I2C (1 pins) for display
 // Analog 4 (27)
-U8G2_SH1106_128X64_NONAME_F_HW_I2C oled(U8G2_R0);
+U8G2_SH1106_128X64_NONAME_1_HW_I2C oled(U8G2_R0);
 
 void setup() {
   // Set PC0-PC2 as analog inputs
 
-//  calibrate();
-   
+  //  calibrate();
+
   // Set digital 8 - 13 for digital output (14-19)
   step = MAX_STEPS;
   motorStates = 0;
-  pinMode(8, OUTPUT);          
-  pinMode(9, OUTPUT);          
-  pinMode(10, OUTPUT);          
-  pinMode(11, OUTPUT);          
-  pinMode(12, OUTPUT);          
-  pinMode(13, OUTPUT);          
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 
   // Set 2-7 for digital input (4-6, 11-13)
   pinMode(2, INPUT);
@@ -51,6 +55,7 @@ void setup() {
   pinMode(6, INPUT);
   pinMode(7, INPUT);
 
+  adc.begin();
   // I2C
   oled.begin();
   oled.setFont(u8g2_font_7x14_tf);
@@ -58,9 +63,30 @@ void setup() {
 }
 
 void loop() {
-  readEncoders();
-  readJoystick();
-  stepMotors(); 
+  int x = adc.read(0);
+  int y = adc.read(1);
+  int z = adc.read(2);
+  int s = adc.read(6);
+
+  oled.firstPage();
+  do {
+    char buf[12];
+    uint8_t offset, line;
+    line = 10;
+    itoa(x, buf, 10);
+    offset = oled.drawStr(0, line, "x=");
+    offset += oled.drawStr(offset, line, buf);
+    itoa(y, buf, 10);
+    offset = 64;
+    offset += oled.drawStr(offset, line, "y=");
+    oled.drawStr(offset, line, buf);
+    line += 15;
+    offset = 0;
+    itoa(z, buf, 10);
+    offset = oled.drawStr(0, line, "z=");
+    offset += oled.drawStr(offset, line, buf);
+    oled.drawStr(0, 64, "Tilialacus CNC");
+  } while (oled.nextPage());
 }
 
 void displayWelcome() {
@@ -68,7 +94,7 @@ void displayWelcome() {
   do {
     oled.setCursor(0, 25);
     oled.print("Tilialacus CNC");
-  } while(oled.nextPage());
+  } while (oled.nextPage());
 }
 
 void readEncoders() {
@@ -79,109 +105,44 @@ void readEncoders() {
 void stepMotors() {
   // Calculate state of each motor
   if (--step == 0) {
-    xStep = xSpeed;
-    yStep = ySpeed;
     step = MAX_STEPS;
-    c++;
-    if (1==2) {
-      c = 0;
-      oled.firstPage();
-      do {
-        oled.setCursor(0, 25);
-        oled.print(xSpeed);
-        oled.setCursor(30, 25);
-        oled.print(ySpeed);
-        oled.setCursor(60, 25);
-        oled.print(zSpeed);
-        oled.setCursor(0, 45);
-        oled.print((motorStates & 0b00001000) ? '<' : '>');
-        oled.setCursor(30, 45);
-        oled.print((motorStates & 0b00010000) ? '<' : '>');
-        if (xStep == 0) {
-          // Toggle x
-          motorStates ^= 0b00000001;
-          xStep = xSpeed;    
-        } else {
-          --xStep;
-        }
-        if (yStep == 0) {
-          // Toggle y
-          motorStates ^= 0b00000010;
-          yStep = ySpeed;    
-        } else {
-          --yStep;
-        }
-      } while(oled.nextPage());
+  }
+
+  if (xSpeed > 0) {
+    if (xStep == xSpeed) {
+      // Toggle x
+      motorStates ^= 0b00000001;
+      xStep = 0;
+    } else {
+      ++xStep;
     }
   }
-  
-  if (xStep == 0) {
-    // Toggle x
-    motorStates ^= 0b00000001;
-    xStep = xSpeed;    
-  } else {
-    --xStep;
-  }
-  if (yStep == 0) {
-    // Toggle y
-    motorStates ^= 0b00000010;
-    yStep = ySpeed;    
-  } else {
-    --yStep;
-  }
-/*  
- if (--zStep == 0) {
-    // Toggle z
-    motorStates ^= 0b00000100;
-    zStep = zSpeed;    
-  }
-*/
   PORTB = motorStates;
 }
 
 void readJoystick() {
-  // Lets not do this every time.
-  // Interrupts?
-  if (step & 1) {
-      int read = analogRead(xaxisIn);
-      if (read > 512) {
-        motorStates |= 0b00001000;
-        read = 1023 - read;
-      } else {
-        motorStates &= 0b11110111;
-      }
-      xSpeed = read >> 1;
+  int read = adc.read(0);
+  if (read > 512) {
+    motorStates |= 0b00001000;
+    read = 1023 - read;
   } else {
-      int read = analogRead(yaxisIn);
-      if (read > 512) {
-        motorStates |= 0b00010000;
-        read = 1023 - read;
-      } else {
-        motorStates &= 0b11101111;
-      }
-      ySpeed = read>>1;
+    motorStates &= 0b11110111;
   }
-      /*
-      read = analogRead(zaxisIn);
-      if (read > 512) {
-        motorStates |= 0b00001000;
-        read = 1023 - read;
-      } else {
-        motorStates &= 0b11110111;
-      }
-      zSpeed = read;
-      */
+
+  if (read > 230) {
+    xSpeed = 0;
+  } else {
+    xSpeed = 1;
+    for (int i = 0; i < read; i += 25) {
+      ++xSpeed;
+    }
+  }
 }
 
 void calibrate() {
-  // Read joystick multiple times to get a 0;
-  for (int i = 0; i < 200; i++) {
-    analogRead(xaxisIn);
-    analogRead(yaxisIn);
-    analogRead(zaxisIn);
-  }
 }
 
 void sendDisplay() {
 }
+
 
