e1162878cdbf6f126f8c4bb426ae545f7bd7696c
Roger Lindsjö <roger@tilialacus.net>
2017-12-30 16:32:59 +0100

    Change step speed by factor 4 and use smaller steps on driver

--- src/cnc_router/cnc_router.ino
+++ src/cnc_router/cnc_router.ino
@@ -61,7 +61,7 @@ void setup() {
   TCCR2B = 0; // same for TCCR2B
   TCNT2  = 0; //initialize counter value to 0
   // set compare match register for 8khz increments
-  OCR2A = 249; // = (16*10^6) / (8000*8) - 1 (must be <256)
+  OCR2A = 61; // = (16*10^6) / (8000*32) - 1 (must be <256)
   TCCR2A |= (1 << WGM21); // turn on CTC mode
   TCCR2B |= (1 << CS21); // Set CS21 bit for 8 prescaler
   TIMSK2 |= (1 << OCIE2A); // enable timer compare interrupt
